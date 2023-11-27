206ed82c1cde306daf537e1e518ad2b09ed8c0d7
Braynner Marques <123399239+BraynnerM@users.noreply.github.com>
2023-11-27 16:27:15 -0300

    Create 2861.js

---
+++ 2861.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+ let n = parseInt(lines[0])
+for (let i =0; i < n; i++) {
+    console.log("gzuz")
+}
